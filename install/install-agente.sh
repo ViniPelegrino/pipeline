475b75640a7c5ab485e02fb40d532f87a96a0fab
claudiosilva@duzeru.org <claudiosilva@duzeru.org>
2022-03-25 18:40:05 +0000

    Update install-agente.sh

--- install/install-agente.sh
+++ install/install-agente.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+sudo curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+sudo apt-get install gitlab-runner
+
+gitlab-runner register -n \
+  --url https://gitlab.com/ \
+  --registration-token GR1348941m4QoXsa6xYQNczyX4xjH \
+  --executor shell \
+  --description "Runner Shell"
