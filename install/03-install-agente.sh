064141e9c2fdbb7a7a6385eabb11e1fa519c7c46
claudio.antonio <claudio.antonio@anvisa.gov.br>
2022-03-28 10:34:51 -0300

    executar scripts

--- install/03-install-agente.sh
+++ install/03-install-agente.sh
@@ -9,28 +9,3 @@ sudo usermod -aG docker gitlab-runner
 
 # Adicionar o gitlab-runner ao sudo sem pedir senha
 sudo echo "gitlab-runner    ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
-
-# Ingressar o Runner em modo shell no GitLab
-sudo gitlab-runner register -n \
-  --url https://gitlab.com/ \
-  --registration-token GR1348941m4QoXsa6xYQNczyX4xjH \
-  --executor shell \
-  --description "Runner Shell"
-
-# Criar o container gitlab-runner para Docker
-docker run -dit \
-  --name runner-docker \
-  --restart always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /opt/gitlab-runner/config:/etc/gitlab-runner \
-  gitlab/gitlab-runner:ubuntu-v12.1.0-rc1
-
-# Ingressar o Runner em modo docker no GitLab
-docker exec -it runner-docker \
-gitlab-runner register -n \
-  --url https://gitlab.com/ \
-  --registration-token GR1348941m4QoXsa6xYQNczyX4xjH \
-  --clone-url http://172.20.0.85 \
-  --executor docker \
-  --docker-image "docker:latest" \
-  --docker-privileged
